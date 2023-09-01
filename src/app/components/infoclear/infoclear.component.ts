3e0630f116f2a7645b062385a8d0d341c238347b
tsy <tsy@mail.com>
2023-09-01 21:05:16 +0300

    feat: infoclear component + mobile version

--- src/app/components/infoclear/infoclear.component.ts
+++ src/app/components/infoclear/infoclear.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-infoclear',
-  templateUrl: './infoclear.component.html',
-  styleUrls: ['./infoclear.component.scss']
+	selector: 'app-infoclear',
+	templateUrl: './infoclear.component.html',
+	styleUrls: ['./infoclear.component.scss'],
 })
-export class InfoclearComponent {
-
-}
+export class InfoclearComponent {}
